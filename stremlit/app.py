bf5731337d7b6f4802729456bd3ce454a4ff0363
chxh514 <Yjoey817@gmail.com>
2024-07-25 17:05:52 +0800

    Update app.py

--- stremlit/app.py
+++ stremlit/app.py
@@ -1,172 +1,197 @@
+# Core Pkgs
 import streamlit as st
-import datetime
-import time
+st.set_page_config(page_title="NLP Web App", page_icon="👍", layout="centered", initial_sidebar_state="auto")
+
+# Viz Pkgs
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
 from wordcloud import WordCloud
+from deep_translator import GoogleTranslator
+
+# NLP Pkgs
 from textblob import TextBlob
 import neattext as nt
 import spacy
-from deep_translator import GoogleTranslator
+
 from collections import Counter
 import re
 
-# 定義 summarize_text 函數
+
+# Sumarization Function
 def summarize_text(text, num_sentences=3):
+    # Remove special characters and convert text to lowercase
     clean_text = re.sub('[^a-zA-Z]', ' ', text).lower()
+    
+    # Split the text into words
     words = clean_text.split()
+    
+    # Calculate the frequency of each word
     word_freq = Counter(words)
+    
+    # Sort the words based on their frequency in descending order
     sorted_words = sorted(word_freq, key=word_freq.get, reverse=True)
+    
+    # Extract the top `num_sentences` most frequent words
     top_words = sorted_words[:num_sentences]
+    
+    # Create the summary by joining the top words
     summary = ' '.join(top_words)
+    
     return summary
 
-# 定義 text_analyzer 函數
-@st.cache_data
+#@st.cache_data  ########
+# Lemma and Tokens Function
 def text_analyzer(text):
+    # import English library
     nlp = spacy.load('en_core_web_sm')
-    print("Model loaded successfully!")
+    # create an nlp object
     doc = nlp(text)
+    #extract tokens and lemmas
     allData = [('"Token":{},\n"Lemma":{}'.format(token.text, token.lemma_)) for token in doc]
     return allData
 
-# 定義 main 函數
+
 def main():
-    """使用 Streamlit 構建的 NLP 網路應用程序"""
+  """NLP web app with Streamlit"""
 
-    title_template = """
-    <div style="background-color:blue; padding:8px;">
-    <h1 style="color:cyan">NLP Web App</h1>
-    </div>
-    """
-    st.markdown(title_template, unsafe_allow_html=True)
+  title_template = """
+  <div style="background-color:blue; padding:8px;">
+  <h1 style="color:cyan">NLP Web App</h1>
+  </div>
+  """
 
-    subheader_template = """
-    <div style="background-color:cyan; padding:8px;">
-    <h3 style="color:blue">Powered by Streamlit</h1>
-    </div>
-    """
-    st.markdown(subheader_template, unsafe_allow_html=True)
+  st.markdown(title_template, unsafe_allow_html=True)
 
-    st.sidebar.image("stremlit/nlp.jpg", use_column_width=True)
+  subheader_template = """
+  <div style="background-color:cyan; padding:8px;">
+  <h3 style="color:blue">Powered by Streamlit</h1>
+  </div>
+  """
 
-    activity = ["Text Analysis", "Translation", "Sentiment Analysis", "About"]
-    choice = st.sidebar.selectbox("Menu", activity, key="menu_selectbox")
+  st.markdown(subheader_template, unsafe_allow_html=True)
 
-    if choice == "Text Analysis":
-        st.subheader("Text Analysis")
-        st.write("")
+  st.sidebar.image("nlp.jpg", use_column_width=True)
 
-        raw_text = st.text_area("Write something", "Enter a text in English...", height=200, key="text_area")
+  activity = ["Text Analysis", "Translation", "Sentiment Analysis", "About"] 
+  choice = st.sidebar.selectbox("Menu", activity)
 
-        if st.button("Analyze", key="analyze_button"):
-            if len(raw_text) == 0:
-                st.warning("Enter a text...")
-            else:
-                st.info("Basic Functions")
+  if choice == "Text Analysis":
+    st.subheader("Text Analysis")
+    st.write("")
 
-                col1, col2 = st.columns(2)
+    raw_text = st.text_area("Write something", "Enter a text in English...", height=350)
 
-                with col1:
-                    with st.expander("Basic Info"):
-                        st.info("Text Stats")
-                        word_desc = nt.TextFrame(raw_text).word_stats()
-                        result_desc = {"Length of Text": word_desc['Length of Text'],
-                                       "Num of Vowels": word_desc['Num of Vowels'],
-                                       "Num of Consonants": word_desc['Num of Consonants'],
-                                       "Num of Stopwords": word_desc['Num of Stopwords']}
-                        st.write(result_desc)
+    if st.button("Analyze"):
+      if len(raw_text) == 0:
+        st.warning("Enter a text...")
+      else:
+        #blob = TextBlob(raw_text)  #######
+        st.info("Basic Function")
 
-                    with st.expander("Stopwords"):
-                        st.success("Stop Words List")
-                        stop_w = nt.TextExtractor(raw_text).extract_stopwords()
-                        st.error(stop_w)
+        col1, col2 = st.columns(2)
 
-                with col2:
-                    with st.expander("Processed Text"):
-                        st.success("Stopwords Excluded Text")
-                        processed_text = str(nt.TextFrame(raw_text).remove_stopwords())
-                        st.write(processed_text)
+        with col1:
+          with st.expander("Basic Info"):
+            st.info("Text Stats")
+            word_desc = nt.TextFrame(raw_text).word_stats()
+            result_desc = {"Length of Text":word_desc['Length of Text'],
+                          "Num of Vowels":word_desc['Num of Vowels'],
+                          "Num of Consonants":word_desc['Num of Consonants'],
+                          "Num of Stopwords":word_desc['Num of Stopwords']}
+            st.write(result_desc)
 
-                    with st.expander("Plot Wordcloud"):
-                        st.success("Wordcloud")
-                        wordcloud = WordCloud().generate(processed_text)
-                        fig = plt.figure(1, figsize=(20, 10))
-                        plt.imshow(wordcloud, interpolation='bilinear')
-                        plt.axis('off')
-                        st.pyplot(fig)
+          with st.expander("Stopwords"):
+            st.success("Stop Words List")
+            stop_w = nt.TextExtractor(raw_text).extract_stopwords()
+            st.error(stop_w)
 
-                st.write("")
-                st.write("")
-                st.info("Advanced Features")
+        with col2:
+          with st.expander("Processed Text"):
+            st.success("Stopwords Excluded Text")
+            processed_text = str(nt.TextFrame(raw_text).remove_stopwords())
+            st.write(processed_text)
 
-                col3, col4 = st.columns(2)
+          with st.expander("Plot Wordcloud"):
+            st.success("Wordcloud")
+            wordcloud = WordCloud().generate(processed_text)
+            fig = plt.figure(1, figsize=(20,10))
+            plt.imshow(wordcloud, interpolation = 'bilinear')
+            plt.axis('off')
+            st.pyplot(fig)
 
-                with col3:
-                    with st.expander("Tokens&Lemmas"):
-                        st.write("T&K")
-                        processed_text_mid = str(nt.TextFrame(raw_text).remove_stopwords())
-                        processed_text_mid = str(nt.TextFrame(processed_text_mid).remove_puncts())
-                        processed_text_fin = str(nt.TextFrame(processed_text_mid).remove_special_characters())
-                        tandl = text_analyzer(processed_text_fin)
-                        st.json(tandl)
-
-                with col4:
-                    with st.expander("Summarize"):
-                        st.success("Summarize")
-                        summary = summarize_text(raw_text)
-                        st.success(summary)
-
-    if choice == "Translation":
-        st.subheader("Translation")
         st.write("")
         st.write("")
-        raw_text = st.text_area("Original Text", "Write something to be translated...", height=200, key="translation_text_area")
-        if len(raw_text) < 3:
-            st.warning("Please provide a text with at least 3 characters...")
-        else:
-            target_lang = st.selectbox("Target Language", ["German","Chinese","English","Spanish", "French", "Italian"], key="target_language_selectbox")
-            if target_lang == "German":
-                target_lang = "de"
-            elif target_lang == "Chinese":
-                target_lang = "zh-TW"
-            elif target_lang == "English":
-                target_lang = "en"
-            elif target_lang == "Spanish":
-                target_lang = "es"
-            elif target_lang == "French":
-                target_lang = "fr"
-            else:
-                target_lang = "it"
+        st.info("Advanced Features")
 
-            if st.button("Translate", key="translate_button"):
-                translator = GoogleTranslator(source='auto', target=target_lang)
-                translated_text = translator.translate(raw_text)
-                st.write(translated_text)
+        col3, col4 = st.columns(2)
 
-    if choice == "Sentiment Analysis":
-        st.subheader("Sentiment Analysis")
+        with col3:
+          with st.expander("Tokens&Lemmas"):
+            st.write("T&K")
+            processed_text_mid = str(nt.TextFrame(raw_text).remove_stopwords())
+            processed_text_mid = str(nt.TextFrame(processed_text_mid).remove_puncts())
+            processed_text_fin = str(nt.TextFrame(processed_text_mid).remove_special_characters())
+            tandl = text_analyzer(processed_text_fin)
+            st.json(tandl)
+
+        with col4:
+          with st.expander("Summarize"):
+            st.success("Summarize")
+            summary = summarize_text(raw_text)
+            st.success(summary)
+
+
+  if choice == "Translation":
+    st.subheader("Translation")
+    st.write("")
+    st.write("")
+    raw_text = st.text_area("Original Text", "Write something to be translated...", height=200)
+    if len(raw_text) < 3:
+      st.warning("Please provide a text with at least 3 characters...")
+    else:
+      target_lang = st.selectbox("Target Language", ["German", "Spanish", "French", "Italian"])
+      if target_lang == "German":
+        target_lang = "de"
+      elif target_lang == "Spanish":
+        target_lang = "es"
+      elif target_lang == "French":
+        target_lang = "fr"      
+      else:
+        target_lang = "it" 
+
+    if st.button("Translate"):
+        translator = GoogleTranslator(source='auto', target=target_lang)  # set source and target languages
+        translated_text = translator.translate(raw_text)
+        st.write(translated_text)
+
+  
+  if choice == "Sentiment Analysis":
+    st.subheader("Sentiment Analysis")
+    st.write("")
+    st.write("")
+    raw_text = st.text_area("Text to analyse", "Enter a text here....", height=200)
+    if st.button("Evaluate"):
+      if len(raw_text) == 0:
+        st.warning("Enter a text...")
+      else:
+        blob = TextBlob(raw_text)
+        st.info("Sentiment Analysis")
+        st.write(blob.sentiment)
         st.write("")
-        st.write("")
-        raw_text = st.text_area("Text to analyse", "Enter a text here....", height=200, key="sentiment_text_area")
-        if st.button("Evaluate", key="evaluate_button"):
-            if len(raw_text) == 0:
-                st.warning("Enter a text...")
-            else:
-                blob = TextBlob(raw_text)
-                st.info("Sentiment Analysis")
-                st.write(blob.sentiment)
-                st.write("")
 
-    if choice == "About":
-        st.subheader("About")
-        st.write("")
+  
+  if choice == "About":
+    st.subheader("About")
+    st.write("")
 
-        st.markdown("""
-        ### NLP Web App made with Streamlit
+    st.markdown("""
+    ### NLP Web App made with Streamlit
+    
+    for info:
+    - [streamlit](https://streamlit.io)
+    """)
 
-        for info:
-        - [streamlit](https://streamlit.io)
-        """)
 
 if __name__ == "__main__":
-    main()
+  main()
