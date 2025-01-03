3340652ebc32cf11e3e37ad57492655ce7936d5e
chxh514 <Yjoey817@gmail.com>
2025-01-03 14:51:34 +0800

    Update 20250103.py

--- stremlit/20250103.py
+++ stremlit/20250103.py
@@ -277,10 +277,9 @@ def main():
     with tabs[3]:
         if st.session_state.analysis_results is not None:
             st.header("Results Visualization")
-
+        total_specific_instances_C = len(specific_instances_C)
             #桑基圖
             # 使用 dynamic choice 生成選項
-        total_specific_instances_C = len(specific_instances_C)
         choices = [f"Data {i+1}" for i in range(total_specific_instances_C)]
         choice = st.selectbox("Data", [" "] + choices)
         
